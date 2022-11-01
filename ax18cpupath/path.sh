b00d4f6a49a69f586e8913440eb1c453b81b1bf9
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2022-11-01 09:16:06 +0800

    Update path.sh

--- ax18cpupath/path.sh
+++ ax18cpupath/path.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
 #调整CPU频率
- cd openwrt/target/linux/ipq60xx/files-4.4/arch/arm64/boot/dts/qcom/
- rm -rf qcom-ipq6000-cpu-opp.dtsi
- wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/qcom-ipq6000-cpu-opp.dtsi
- cd
+ cd openwrt/
+ rm -rf target/linux/ipq60xx/files-4.4/arch/arm64/boot/dts/qcom/qcom-ipq6000-cpu-opp.dtsi
+ wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/qcom-ipq6000-cpu-opp.dtsi target/linux/ipq60xx/files-4.4/arch/arm64/boot/dts/qcom/
  
 #添加桥管理
- cd openwrt/
  rm -rf target/linux/ipq60xx/Makefile
  wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/Makefile target/linux/ipq60xx/
- cd
 
 #dnsmasq：默认不过滤 AAAA 记录
- cd openwrt/
  rm -rf package/network/services/dnsmasq/files/dhcp.conf
  wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/dhcp.conf package/network/services/dnsmasq/files/
- cd
 
 #tcp 65535 
- cd openwrt/
  rm -rf package/kernel/linux/files/sysctl-nf-conntrack.conf
  wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/sysctl-nf-conntrack.conf package/kernel/linux/files/
- cd
  
 #禁用nss网桥
- cd openwrt/
  rm -rf package/qca/qca-nss-ecm/files/qca-nss-ecm.init
  wget https://raw.githubusercontent.com/lauren12133/OpenWrt-Build/main/ax18cpupath/qca-nss-ecm.init package/qca/qca-nss-ecm/files/
- cd
